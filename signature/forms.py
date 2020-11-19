594246ff442ffac279b9b3b66112b4feebbd49f9
shunt <wayneculverhall@gmail.com>
2020-11-19 10:02:20 -0800

    blah

--- signature/forms.py
+++ signature/forms.py
@@ -5,4 +5,7 @@ from .models import EmployeeInfo
 class SignatureForm(ModelForm):
     class Meta:
         model = EmployeeInfo
-        fields = ['employee_first_name', 'employee_last_name', 'employee_title', 'employee_area_code','employee_phone_number', 'employee_pronoun_list', 'employee_address1', 'employee_address2', 'employee_zoomid', 'employee_profile_pic']
+        fields = ['employee_first_name', 'employee_last_name', 'employee_title', 
+        'employee_area_code','employee_phone_number', 'employee_pronoun_list', 'employee_pronoun_other', 
+        'employee_address', 
+        'employee_address_other1', 'employee_address_other2', 'employee_zoomid', 'employee_profile_pic']
--- signature/models.py
+++ signature/models.py
@@ -7,16 +7,29 @@ class EmployeeInfo(models.Model):
         ('a', "They / Them"),
         ('b', "She / Her / Hers"),
         ('c', "He / Him / His"),
+        ('d', "Other, please specify"),
     )
+    ADDRESS_LIST = (
+        ('a', "9500 NE Cascades Pkwy, Portland, OR 97220"),
+        ('b', "17014 NE Sandy Blvd., Portland, OR 97230"),
+        ('c', "508 S. Boston Ave., Tulsa, OK 74103"),
+        ('d', "Portland, Oregon"),
+        ('e', "Tulsa, Oklahoma"),
+        ('f', "Other, please specify"),
+    )
+
     employee_first_name = models.CharField(max_length=50)
     employee_title = models.CharField(max_length=100)
     employee_last_name = models.CharField(max_length=50)
     employee_area_code = models.CharField(max_length=3)
     employee_phone_number = models.CharField(max_length=50)
-    employee_pronoun_list = models.CharField( max_length=1, choices=PRONOUN_CHOICES)
-    employee_address1 = models.CharField(max_length=50)
-    employee_address2 = models.CharField(max_length=50)
-    employee_zoomid = models.CharField(max_length=50)
+    employee_pronoun_list = models.CharField( max_length=1, choices=PRONOUN_CHOICES, blank=True)
+    employee_pronoun_other = models.CharField(max_length=50, blank=True)
+    employee_address = models.CharField(max_length=1, choices=ADDRESS_LIST, blank=True )
+    employee_address_other1 = models.CharField(max_length=100, blank=True)
+    employee_address_other2 = models.CharField(max_length=100, blank=True)
+
+    employee_zoomid = models.CharField(max_length=50, blank=True)
     employee_profile_pic = models.ImageField(null=True, blank=True)
     url = models.SlugField(max_length=500,blank=True)
 
@@ -24,10 +37,10 @@ class EmployeeInfo(models.Model):
     def save(self, *args, **kwargs):
         self.url = slugify(self.employee_first_name + self.employee_last_name + self.employee_title)
         super().save(*args, **kwargs)
-        img = Image.open(self.employee_profile_pic.path)
+        if self.employee_profile_pic:
+            img = Image.open(self.employee_profile_pic.path)
 
-        if img.height > 120 or img.weight > 120:
-            output_size = (120, 120)
-            img.thumbnail(output_size)
-            img.save(self.employee_profile_pic.path)
-
+            if img.height > 120 or img.weight > 120:
+                output_size = (120, 120)
+                img.thumbnail(output_size)
+                img.save(self.employee_profile_pic.path)
