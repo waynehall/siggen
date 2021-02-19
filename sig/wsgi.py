7385fe9f47802100f09a058fbd3c06a5699f085e
shunt <wayneculverhall@gmail.com>
2021-02-19 08:26:05 -0800

    new

--- sig/wsgi.py
+++ sig/wsgi.py
@@ -7,10 +7,14 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
+import os 
+import sys 
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sig.settings')
+sys.path.append('home/ubuntu/siggen')
+sys.path.append('home/ubuntu/siggen/sig')
+
 
 application = get_wsgi_application()
